bd7ae16b2fb7738f8a2ba1ff375c2fa7b9305628
Thomas Wolter <wolter@cheaphotel.de>
2014-01-29 17:43:29 +0100

    Noch mehr Dampf

--- js/tom.js
+++ js/tom.js
@@ -1,8 +1,19 @@
-$(function(){  	
+function adjust_to_screen() {
 	var breite = $(window).innerWidth()*0.95;
 	var hoehe =  $(window).innerHeight()*0.95;
     $("#thevideo").css("width", breite);
     $("#thevideo").css("height", hoehe);
     $("#thevideo").css("margin-left", -(breite/2));
-    $("#thevideo").css("margin-top", -(hoehe/2));
+    $("#thevideo").css("margin-top", -(hoehe/2));	
+}
+
+$(function(){  	
+    adjust_to_screen();
+    
+    window.addEventListener("orientationchange", function() {
+    	adjust_to_screen();
+    })   
+    window.addEventListener("resize", function() {
+    	adjust_to_screen();
+    }) 
 })           
